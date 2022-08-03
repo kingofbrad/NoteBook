9070fe3e6e847f3d60a8da7edb87b2e4baf449ee
kingofbrad <82820990+kingofbrad@users.noreply.github.com>
2022-08-03 13:54:50 +0100

    comment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Flex, Heading, IconButton, useColorMode } from "@chakra-ui/react";
 import React, { useState, useEffect } from "react";
 import MainNoteApp from "./Pages/MainNoteApp";
 import MainReminderApp from "./Pages/MainReminderApp";
-import ErrorPage from "./Pages/errorpage";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard";
 function App() {
